dd6bd8ea1a91fecdbbdee74bf4847fd32d3e5370
Vasile Minica <69588689+vmbs@users.noreply.github.com>
2024-05-24 16:12:04 +0100

    Update run.sh with extra config options

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Check if DISCORD_TOKEN and OWNER_ID are set
 if [ -z "$DISCORD_TOKEN" ]; then
   echo "Error: DISCORD_TOKEN is not set."
   exit 1
@@ -11,11 +10,61 @@ if [ -z "$OWNER_ID" ]; then
   exit 1
 fi
 
-# Create the configuration file
 cat <<EOT > config.txt
 token = $DISCORD_TOKEN
 owner = $OWNER_ID
+prefix = "${PREFIX:-!}"
+game = "${GAME:-DEFAULT}"
+status = ${STATUS:-ONLINE}
+songinstatus = ${SONGINSTATUS:-false}
+altprefix = "${ALTPREFIX:-NONE}"
+success = "${SUCCESS_EMOJI:-🎶}"
+warning = "${WARNING_EMOJI:-💡}"
+error = "${ERROR_EMOJI:-🚫}"
+loading = "${LOADING_EMOJI:-⌚}"
+searching = "${SEARCHING_EMOJI:-🔎}"
+help = ${HELP:-help}
+npimages = ${NPIMAGES:-false}
+stayinchannel = ${STAYINCHANNEL:-false}
+maxtime = ${MAXTIME:-0}
+maxytplaylistpages = ${MAXYTPLAYLISTPAGES:-10}
+skipratio = ${SKIPRATIO:-0.55}
+alonetimeuntilstop = ${ALONETIMEUNTILSTOP:-0}
+playlistsfolder = "${PLAYLISTSFOLDER:-Playlists}"
+updatealerts = ${UPDATEALERTS:-true}
+lyrics.default = "${LYRICS_PROVIDER:-A-Z Lyrics}"
+aliases {
+  settings = [ status ]
+  lyrics = []
+  nowplaying = [ np, current ]
+  play = []
+  playlists = [ pls ]
+  queue = [ list ]
+  remove = [ delete ]
+  scsearch = []
+  search = [ ytsearch ]
+  shuffle = []
+  skip = [ voteskip ]
+  prefix = [ setprefix ]
+  setdj = []
+  setskip = [ setskippercent, skippercent, setskipratio ]
+  settc = []
+  setvc = []
+  forceremove = [ forcedelete, modremove, moddelete, modelete ]
+  forceskip = [ modskip ]
+  movetrack = [ move ]
+  pause = []
+  playnext = []
+  queuetype = []
+  repeat = []
+  skipto = [ jumpto ]
+  stop = [ leave ]
+  volume = [ vol ]
+}
+loglevel = ${LOGLEVEL:-info}
+transforms = {}
+eval = ${EVAL:-false}
+evalengine = "${EVALENGINE:-Nashorn}"
 EOT
 
-# Run the JMusicBot
-java -jar JMusicBot.jar
+java -Dnogui=true -jar JMusicBot.jar
